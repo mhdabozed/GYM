1fdaf0745b89a90b76113be5dc42b57fb9bdeacd
mhdabozed <mhdabozed@gmail.com>
2023-12-29 16:41:09 +0300

    creat ente method

--- src/gym/GYM.java
+++ src/gym/GYM.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package gym;
+import java.util.Scanner;
 
 /**
  *
@@ -11,11 +12,37 @@ package gym;
  */
 public class GYM {
 
+
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        Scanner scan = new Scanner(System.in);
+
+        String last_name = null;
+        String first_name = null;
+        String gender = null;
+        String phone = null;
+        String username = null;
+        String password = null;
+        empolyees e1 = new empolyees(first_name, last_name, gender, phone, username, password);
+        String i;
+        System.out.println("what you want to add[em,cl,sp]");
+        i = scan.nextLine();
+        if (i == "em") {
+            e1.ente();
+
+        }
+    }}
+
+
+
+
+
+
+
+
     
-}
+
